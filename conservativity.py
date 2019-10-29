cf67a0ed9709f98f1ab76bafa8ea302b7710fa4a
Cauã Roca Antunes <crantunes@inf.ufrgs.br>
2019-10-29 15:32:03 -0300

    Optmized correction - debugging phase

--- conservativity.py
+++ conservativity.py
@@ -16,13 +16,13 @@ def correct(v,o1,o2):
     l1 = len(o1.base_iri)
     l2 = len(o2.base_iri)
     size = 0
-    for s in v.subjects(RDF.type, alignCell):
+    for s in v.subjects(RDF.type, alignCell):   # n
         size += 1
         e1 = v.value(s,alignEntity1, None)[l1:]
         e2 = v.value(s,alignEntity2, None)[l2:]
         violation = False
-        for e in a:
-            if o1[e[0]] in o1[e1].ancestors():
+        for e in a:     # 1..n
+            if o1[e[0]] in o1[e1].ancestors():  # n
                 if o2[e[1]] not in o2[e2].ancestors():
                     violation = True
             else:
@@ -42,6 +42,91 @@ def correct(v,o1,o2):
         if not violation:
            a.append((e1,e2,s))
     return (removedNodes, size, len(a))
+
+
+def correct_opt(v,o1,o2):
+    aligned1 = {}
+    aligned1["Thing"] = None
+    aligned1["Property"] = None
+    aligned1["InverseFunctionalProperty"] = None
+    aligned1["FunctionalProperty"] = None
+    aligned1["ObjectProperty"] = None
+    aligned1["DataProperty"] = None
+    for c in o1.classes():
+        aligned1[c.name] = None
+    for c in o1.object_properties():
+        aligned1[c.name] = None
+
+    aligned2 = {}
+    aligned2["Thing"] = None
+    aligned2["Property"] = None
+    aligned2["InverseFunctionalProperty"] = None
+    aligned2["FunctionalProperty"] = None
+    aligned2["ObjectProperty"] = None
+    aligned2["DataProperty"] = None
+    for c in o2.classes():
+        aligned2[c.name] = None
+    for c in o2.object_properties():
+        aligned2[c.name] = None
+
+    tx1 = {}
+    tx2 = {}
+    l = {}
+    
+    l1 = len(o1.base_iri)
+    l2 = len(o2.base_iri)
+    for s in v.subjects(RDF.type, alignCell):
+        e1 = v.value(s,alignEntity1, None)[l1:]
+        e2 = v.value(s,alignEntity2, None)[l2:]
+        aligned1[e1] = s
+        aligned2[e2] = s
+        tx1[s] = {}
+        tx2[s] = {}
+        for i in l:
+            tx1[s][i] = False
+            tx2[s][i] = False
+            tx1[i][s] = False
+            tx2[i][s] = False
+        tx1[s][s] = True
+        tx2[s][s] = True
+        l[s]=(s,e1,e2)
+    
+    t0 = time.process_time()#
+    droplist = []
+    for i in l:
+        ln = []
+        for y in o1[l[i][1]].ancestors():
+            s = aligned1[y.name]
+            if s != None:
+                tx1[i][s] = True
+                ln.append(y.name)
+        for y in o2[l[i][2]].ancestors():
+            s = aligned2[y.name]
+            if s != None:
+                tx2[i][s] = True
+                if not tx1[i][s]:
+                    droplist.append(l[i][0])
+                    droplist.append(s)
+                    aligned1[l[i][1]] = None
+                    aligned1[l[s][1]] = None
+                    aligned2[l[i][2]] = None
+                    aligned2[l[s][2]] = None
+                    break
+        if aligned1[l[i][1]] != None:
+            for y in ln:
+                s = aligned1[y]
+                if s != None and not tx2[i][s]:
+                    droplist.append(l[i][0])
+                    droplist.append(s)
+                    aligned1[l[i][1]] = None
+                    aligned1[l[s][1]] = None
+                    aligned2[l[i][2]] = None
+                    aligned2[l[s][2]] = None
+                    break
+    t1 = time.process_time()#
+    print(t1-t0)#
+    return droplist
+
 
 def findViolations(v,o1,o2):
     a = []
@@ -93,6 +178,17 @@ def evaluateAlignment(fileName):
     o2 = get_ontology(onto2.lower()[7:]+".owl")
     o1.load()
     o2.load()
+
+    #debugging
+    t0 = time.process_time()
+    #correct(v,o1,o2)
+    t1 = time.process_time()
+    #print(t1-t0)
+    correct_opt(v,o1,o2)
+    t2 = time.process_time()
+    print(t2-t1)
+    return()
+    #end debugging
 
     t0 = time.process_time()
     violations = findViolations(v,o1,o2)
